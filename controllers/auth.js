2c0e880646d72d0f4e332a27062f48d86b902c02
larasodjati <108501073+larasodjati@users.noreply.github.com>
2022-12-19 18:55:25 -0800

    answers for lesson week9

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,22 +1,28 @@
 const User = require('../models/User');
 const {StatusCodes} = require('http-status-codes');
 const {BadRequestError} = require('../errors');
-const bcrypt = require('bcryptjs');
+//const bcrypt = require('bcryptjs');--> can be removed bcs we alr set up in models
+const jwt = require('jsonwebtoken');
 
 const register = async (req, res) => {
     /*const {name, email, password} = req.body
     if(!name || !email|| !password){
         throw new BadRequestError('Please provide name, email, and password')
     }*/
-    const {name, email, password} = req.body;
+    //const {name, email, password} = req.body;
 
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt)
+    //const salt = await bcrypt.genSalt(10);
+    //const hashedPassword = await bcrypt.hash(password, salt)
 
-    const tempUser = {name, email, password:hashedPassword};
+    //const tempUser = {name, email, password:hashedPassword}
 
-    const user = await User.create({...tempUser});
-    res.status(StatusCodes.CREATED).json({user});
+    const user = await User.create({...req.body})
+    const token = jwt.sign({userId:user._id, name:user.name}, 'jwtSecret',{
+        expiresIn: '30d',
+    })
+    res
+    .status(StatusCodes.CREATED)
+    .json({user:{name: user.getName()}, token});
 }
 
 const login = async (req, res) => {
